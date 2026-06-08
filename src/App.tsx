0662717d66c1b327628a6c184e60a82f1617f127
PrinceTyo <kresnaale2@gmail.com>
2026-06-08 19:30:05 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import MainLayout from "@/layouts/MainLayout";
+import Home from "@/pages/home";
+
+export default function App() {
+  return (
+    <MainLayout>
+      <Home />
+    </MainLayout>
+  );
+}
---
+++ src/sections/hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <div className="">
+      <h1>hero section</h1>
+    </div>
+  );
+}
