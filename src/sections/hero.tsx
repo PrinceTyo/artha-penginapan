d7063ed862d94c6eba4f4b6bdf94bb665237f9d9
PrinceTyo <kresnaale2@gmail.com>
2026-06-10 07:39:54 +0700

    fix hero-section

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,17 +1,34 @@
-import hero1 from "@/assets/hero.png";
+import { useEffect, useState } from "react";
+import hero1 from "@/assets/hero1.png";
+import hero2 from "@/assets/hero2.png";
+import hero3 from "@/assets/hero3.png";
 import { IoIosArrowForward } from "react-icons/io";
 
 export default function Hero() {
+  const images = [hero1, hero2, hero3];
+  const [currentSlide, setCurrentSlide] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev + 1) % images.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <section
-      className="relative min-h-screen flex justify-center lg:justify-start items-center px-4 lg:px-30 text-white"
-      style={{
-        backgroundImage: `url(${hero1})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      <div className="lg:max-w-2xl flex flex-col items-center text-center lg:text-start lg:items-start">
+    <section className="relative min-h-screen overflow-hidden">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className={`absolute inset-0 bg-cover bg-center transition-opacity duration-1000 ${
+            currentSlide === index ? "opacity-100" : "opacity-0"
+          }`}
+          style={{
+            backgroundImage: `url(${image})`,
+          }}
+        />
+      ))}
+      <div className="relative z-10 lg:max-w-2xl top-60 -right-30 text-center lg:text-start lg:items-start">
         <h1 className="text-white text-4xl md:text-5xl font-light">
           Switch Life to Artha
         </h1>
@@ -28,6 +45,28 @@ export default function Hero() {
             Explore More <IoIosArrowForward />
           </button>
         </a>
+      </div>
+      <div className="absolute right-8 top-1/2 -translate-y-1/2 flex flex-col items-center gap-2.5">
+        {images.map((_, index) => (
+          <label
+            key={index}
+            className="cursor-pointer flex items-center justify-center"
+          >
+            <input
+              type="radio"
+              name="hero-slider"
+              checked={currentSlide === index}
+              onChange={() => setCurrentSlide(index)}
+              className="hidden"
+            />
+
+            <div className="w-6 h-6 rounded-full border-2 border-white flex items-center justify-center">
+              {currentSlide === index && (
+                <div className="w-2.5 h-2.5 rounded-full bg-white" />
+              )}
+            </div>
+          </label>
+        ))}
       </div>
     </section>
   );
